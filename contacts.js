561e4d202fdb864e38d82006f548281445441196
N1tr1c <dudecat41@gmail.com>
2025-09-23 17:00:15 -0400

    Added Email Icon

--- contacts.js
+++ contacts.js
@@ -1,5 +1,10 @@
 let contacts = [
     {
+        method: "Email: brassquakegw@gmail.com",
+        url: "",
+        icon: "images/email.png"
+    },
+    {
         method: "YouTube",
         url: "https://www.youtube.com/@Brassquake",
         icon: "images/youtube.png"
@@ -9,19 +14,14 @@ let contacts = [
         url: "https://www.instagram.com/brassquake?igsh=OGw0cXkyOG1xamZ2",
         icon: "images/instagram.png"
     },
-    {
-        method: "Email",
-        url: "https://mail.google.com/mail/u/0/?fs=1&to=brassquakegw@gmail.com",
-        icon: "images/instagram.png"
-    },
 ];
 
 for(let i = 0; i < contacts.length; i++) {
     document.querySelector('#contacts-list').insertAdjacentHTML('beforeend', `
         <li>
             <div style="display: flex; justify-content: center; align-items: center; gap: 10px;">
-                <img src="${contacts[i].icon}" style="width: 30px;">
-                <a style="color: white;" href="${contacts[i].url}" target="_blank" title="${contacts[i].method}">${contacts[i].method}</a>
+                <img src="${contacts[i].icon}" style="width: 30px; border-radius: 5px;">
+                <a style="color: white;" href="${contacts[i].url}" target="${(contacts[i].url) ? '_blank' : ''}" title="${contacts[i].method}">${contacts[i].method}</a>
             </div>
         </li>
     `);
