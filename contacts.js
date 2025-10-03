8ed0223caaa64d5a504cb9c013a92af7e4c035f0
Arvin O. <64636403+arvinstrumpet@users.noreply.github.com>
2025-10-03 10:47:19 -0400

    Update contacts.js

--- contacts.js
+++ contacts.js
@@ -1,34 +1,34 @@
-let contacts = [
+document.addEventListener("DOMContentLoaded", () => {
+  let contacts = [
     {
-        method: "Email: brassquakegw@gmail.com",
-        url: "mailto:brassquakegw@gmail.com",
-        icon: "images/Email.png"
+      method: "Email: brassquakegw@gmail.com",
+      url: "mailto:brassquakegw@gmail.com",
+      icon: "images/Email.png"
     },
     {
-        method: "YouTube - @brassquake",
-        url: "https://www.youtube.com/@Brassquake",
-        icon: "images/youtube.png"
+      method: "YouTube - @brassquake",
+      url: "https://www.youtube.com/@Brassquake",
+      icon: "images/youtube.png"
     },
     {
-        method: "Instagram - @brassquake",
-        url: "https://www.instagram.com/brassquake?igsh=OGw0cXkyOG1xamZ2",
-        icon: "images/instagram.png"
+      method: "Instagram - @brassquake",
+      url: "https://www.instagram.com/brassquake?igsh=OGw0cXkyOG1xamZ2",
+      icon: "images/instagram.png"
     },
-];
+  ];
 
-for (let i = 0; i < contacts.length; i++) {
+  for (let i = 0; i < contacts.length; i++) {
     const contact = contacts[i];
     const isEmail = contact.url.startsWith("mailto:");
 
     document.querySelector('#contacts-list').insertAdjacentHTML('beforeend', `
-        <li>
-            <div style="display: flex; justify-content: center; align-items: center; gap: 10px;">
-                <img src="${contact.icon}" style="width: 30px; border-radius: 5px;">
-                ${isEmail
-                    ? `<span style="color: white;">${contact.method}</span>`
-                    : `<a style="color: white;" href="${contact.url}" target="_blank" title="${contact.method}">${contact.method}</a>`
-                }
-            </div>
-        </li>
+      <li>
+        <img src="${contact.icon}" alt="${contact.method}">
+        ${isEmail
+          ? `<span>${contact.method}</span>`
+          : `<a href="${contact.url}" target="_blank" title="${contact.method}">${contact.method}</a>`
+        }
+      </li>
     `);
-}
+  }
+});
